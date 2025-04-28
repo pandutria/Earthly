f71bad0a9867ccc0012f047fc55356f0a1d5fed8
pandutria <panduadyatama029@gmail.com>
2025-04-28 19:38:01 +0700

    first commit

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-import "./Navbar.css"
-import Logo from "../../assets/images/logo.png";
+    import React from "react";
+    import "./Navbar.css"
+    import Logo from "../../assets/images/logo.png";
 
-function Navbar ({children}) {
-    return (
-        <div className="navbar-container">
+    function Navbar() {
+        return (
             <div className="navbar">
                 <div className="logo-container">
-                     <img src={Logo} alt="logo" />
-                     <h1>Earthly</h1>
+                    <img src={Logo} alt="logo" />
+                    <h1>Earthly</h1>
+                </div>
+                <div className="nav-text">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="" >About Us</a></li>
+                        <li><a href="">E-Commerce</a></li>
+                    </ul>
                 </div>
             </div>
-            <div>
-                {children}
-            </div>
-        </div>
-    )
-}
+        )
+    }
+    
 
-export default Navbar
+    export default Navbar
--- frontend/src/pages/customer/main/Main.jsx
+++ frontend/src/pages/customer/main/Main.jsx
@@ -1,13 +1,52 @@
 import React from "react";
 import Navbar from "../../../components/NavBar/Navbar";
-import "./Main.css"
+import "./Main.css";
+// import home from "../../../assets/images/home.png"
+import homeImage from "../../../assets/images/hone.png";
+import home1 from "../../../assets/images/home1.png";
+import home2 from "../../../assets/images/home2.png";
+import home3 from "../../../assets/images/home3.png";
+import garis from "../../../assets/images/garis.png"
 
-function Main() {
-    return (
-        <Navbar>
-
-        </Navbar>
-    )
-}
+const Main = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <div className="home-section">
+        <div className="home-top">
+          <img src={homeImage} alt="" />
+          <div className="home-right">
+            <h1>Earthly</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.{" "}
+            </p>
+            <a href="">Explore product</a>
+          </div>
+        </div>
+        <div className="home-bottom">
+            <div className="home-bottom-container">
+              <img src={home1} alt="" />
+              <p>Lorem Ipsum dolor sit amet</p>
+          </div>
+          <img src={garis} alt="" />
+          <div className="home-bottom-container">
+              <img src={home2} alt="" />
+              <p>Lorem Ipsum dolor sit amet</p>
+          </div>
+          <img src={garis} alt="" /> 
+          <div className="home-bottom-container">
+              <img src={home3} alt="" />
+              <p>Lorem Ipsum dolor sit amet</p>
+          </div>   
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
