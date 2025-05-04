b31a6b0d51818276ecf088816ff897650c4e2e4d
pandutria <panduadyatama029@gmail.com>
2025-05-04 22:37:05 +0700

    first commit

--- frontend/src/pages/customer/Catalog/Catalog.jsx
+++ frontend/src/pages/customer/Catalog/Catalog.jsx
@@ -1,16 +1,94 @@
 import React from "react";
 import Navbar from "../../../components/NavBar/Navbar";
 import "./Catalog.css";
+import Search from "../../../assets/images/serch.png";
+import { useEffect, useState } from "react";
+import HttpHandler from "../../../data/HttpHandler";
+import star from "../../../assets/images/star.png";
 
 const Catalog = () => {
+  const [categories, setCategories] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
+  const [products, useProducts] = useState([]);
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetchCategoryData();
+  }, []);
+
+  const fetchCategoryData = async () => {
+    try {
+      const res = await HttpHandler.request("categories", "GET");
+      const code = JSON.parse(res).code;
+      const body = JSON.parse(res).body;
+
+      if (code === 200) {
+        const data = JSON.parse(body);
+        setCategories(data);
+      }
+    } catch (err) {
+      console.error("Failed to fetch categories:", err);
+    }
+  };
+
+  const fecthProducts = async () => {
+    try {
+      const url = await HttpHandler.request("products", "GET");
+      const code = JSON.parse(url).code;
+      const body = JSON.parse(url).body;
+
+      if (code === 200) {
+        const data = JSON.parse(body);
+        useProducts(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fecthProducts();
+  }, []);
+
   return (
     <div className="section-catalog">
       <h1>Catalog Products</h1>
       <div className="filter-container">
         <div className="login-input-wrapper">
-          <img src="" alt="user" className="login-input-icon" />
-          <input type="text" placeholder="Write your username" />
+          <img src={Search} alt="user" className="login-input-icon" />
+          <input type="text" placeholder="Search Product..." 
+          value={search}
+          onChange={(x) => setSearch(x.target.value)}/>
         </div>
+        <select
+          value={selectedCategoryId}
+          onChange={(e) => setSelectedCategoryId(e.target.value)}
+        >
+          <option value="">-- Select Category --</option>
+          {categories.map((cat) => (
+            <option key={cat.id} value={cat.id}>
+              {cat.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="product-container">
+        {products.filter((product) => product.name.toLowerCase().includes(search.toLowerCase()) && (selectedCategoryId === "" || product.category.id === Number(selectedCategoryId)))
+        .map((product, index) => (
+          <div className="product-item" key={index}>
+            <img src={product.image_url} alt="" />
+            <div className="product-item-text">
+              <p>{product.category.name}</p>
+              <h1>{product.name}</h1>
+              <div className="product-item-text-rating">
+                <img src={star} alt="" />
+                <h2>(10 Review)</h2>
+              </div>
+              <h3>Rp. {product.price.toLocaleString("id-ID")}</h3>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- frontend/src/pages/customer/main/Main.jsx
+++ frontend/src/pages/customer/main/Main.jsx
@@ -12,41 +12,43 @@ import wonder2 from "../../../assets/images/wonder2.png";
 import wonder3 from "../../../assets/images/wonder3.png";
 import wonder4 from "../../../assets/images/wonder4.png";
 import wonder5 from "../../../assets/images/wonder5.png";
+import after1 from "../../../assets/images/after1.png";
+import after2 from "../../../assets/images/after2.png";
 import star from "../../../assets/images/star.png";
 import test from "../../../assets/images/test.png";
 import { useNavigate } from "react-router-dom";
 import DataStorage from "../../../helper/DataStorage";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
-import HttpHandler from "../../../data/HttpHandler"
+import HttpHandler from "../../../data/HttpHandler";
 import { useEffect } from "react";
 
 const Main = () => {
-  const navigate = useNavigate()
-  const [products, useProducts] = useState([])
+  const navigate = useNavigate();
+  const [products, useProducts] = useState([]);
 
   // if (DataStorage.getToken() === "") {
   //   return <Navigate to="/logi n" replace />;
   // }
 
   const navigateToCatalog = () => {
-    navigate("/main/customer/catalog")
-  }
+    navigate("/main/customer/catalog");
+  };
 
   const fecthProducts = async () => {
     try {
-      const url = await HttpHandler.request("products", "GET")
-      const code = JSON.parse(url).code
-      const body = JSON.parse(url).body
+      const url = await HttpHandler.request("products", "GET");
+      const code = JSON.parse(url).code;
+      const body = JSON.parse(url).body;
 
       if (code === 200) {
-        const data = JSON.parse(body)
-        useProducts(data)
+        const data = JSON.parse(body);
+        useProducts(data);
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
     fecthProducts();
@@ -72,29 +74,36 @@ const Main = () => {
           </div>
         </div>
         <div className="home-bottom">
-            <div className="home-bottom-container">
-              <img src={home1} alt="" />
-              <p>Lorem Ipsum dolor sit amet</p>
+          <div className="home-bottom-container">
+            <img src={home1} alt="" />
+            <p>Lorem Ipsum dolor sit amet</p>
           </div>
           <img src={garis} alt="" />
           <div className="home-bottom-container">
-              <img src={home2} alt="" />
-              <p>Lorem Ipsum dolor sit amet</p>
+            <img src={home2} alt="" />
+            <p>Lorem Ipsum dolor sit amet</p>
           </div>
-          <img src={garis} alt="" /> 
+          <img src={garis} alt="" />
           <div className="home-bottom-container">
-              <img src={home3} alt="" />
-              <p>Lorem Ipsum dolor sit amet</p>
-          </div>   
+            <img src={home3} alt="" />
+            <p>Lorem Ipsum dolor sit amet</p>
+          </div>
         </div>
       </div>
 
       <div className="after-home-section">
         <div className="after-home-text">
           <h1>Lorem Ipsum</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.{" "}
+          </p>
         </div>
-      <img src={afterHome} alt="" />
+        <img src={afterHome} alt="" />
       </div>
 
       <div className="wonder-section">
@@ -104,21 +113,30 @@ const Main = () => {
             <img src={wonder1} alt="" />
             <div className="wonder-item-text">
               <h1>Recycled Plastic</h1>
-              <p>Collected from household waste and crafted into durable bags and accessories.</p>
+              <p>
+                Collected from household waste and crafted into durable bags and
+                accessories.
+              </p>
             </div>
           </div>
           <div className="wonder-item">
-            <img src={wonder2} alt="" style={{top: '-90px'}}/>
+            <img src={wonder2} alt="" style={{ top: "-90px" }} />
             <div className="wonder-item-text">
               <h1>Reclaimed Wood</h1>
-              <p>Salvaged from old buildings to create rustic, sustainable furniture.</p>
+              <p>
+                Salvaged from old buildings to create rustic, sustainable
+                furniture.
+              </p>
             </div>
           </div>
           <div className="wonder-item">
-            <img src={wonder5} alt="" style={{top: '-50px'}} />
+            <img src={wonder5} alt="" style={{ top: "-50px" }} />
             <div className="wonder-item-text">
               <h1>Recycled Fabric</h1>
-              <p>Leftover fabrics from the fashion industry transformed into stylish totes and pouches.</p>
+              <p>
+                Leftover fabrics from the fashion industry transformed into
+                stylish totes and pouches.
+              </p>
             </div>
           </div>
         </div>
@@ -127,24 +145,28 @@ const Main = () => {
       <div className="product-section">
         <h1>Featured Product</h1>
         <div className="product-container">
-          {products.map((product, index) => 
-             <div className="product-item" key={index} onClick={navigateToCatalog}>
-             <img src={product.image_url} alt="" />
-             <div className="product-item-text">
-               <p>{product.category.name}</p>
-               <h1>{product.name}</h1>
-               <div className="product-item-text-rating">
-                 <img src={star} alt="" />
-                 <h2>(10 Review)</h2>
-               </div>
-               <h3>Rp. {product.price.toLocaleString("id-ID")}</h3>
-             </div>
- 
-           </div>
-          )}
+          {products.slice(0, 8).map((product, index) => (
+            <div className="product-item" key={index}>
+              <img src={product.image_url} alt="" />
+              <div className="product-item-text">
+                <p>{product.category.name}</p>
+                <h1>{product.name}</h1>
+                <div className="product-item-text-rating">
+                  <img src={star} alt="" />
+                  <h2>(10 Review)</h2>
+                </div>
+                <h3>Rp. {product.price.toLocaleString("id-ID")}</h3>
+              </div>
+            </div>
+          ))}
         </div>
-
+        <button onClick={navigateToCatalog}>View All</button>
       </div>
+
+          <div className="after-product-section">
+            <img src={after1} alt="" />
+            <img src={after2} alt="" />
+          </div>
 
     </div>
   );
